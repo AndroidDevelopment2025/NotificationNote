ce3955368096fe12841992062094234ddaa2a2e3
andreas-lonn <andreas.lonn@ju.se>
2025-12-03 10:22:15 +0100

    End of lecture
    - Extract notification channel creation and notification posting into their own functions
    - Make list of notes, and post them on startup

--- app/src/main/java/se/andreaslonn/notificationnote/MainActivity.kt
+++ app/src/main/java/se/andreaslonn/notificationnote/MainActivity.kt
@@ -3,6 +3,8 @@ package se.andreaslonn.notificationnote
 import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -24,6 +26,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -31,34 +34,57 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationCompat.EXTRA_NOTIFICATION_ID
 import androidx.core.app.NotificationManagerCompat
 import se.andreaslonn.notificationnote.ui.theme.NotificationNoteTheme
+import kotlin.jvm.java
+import kotlin.random.Random
 
 private const val CHANNEL_ID = "MY_NOTIFICATION_CHANNEL"
+
+data class Note(
+    val id: Int,
+    val title: String,
+    val content: String,
+    val time: Int,
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel()
+        }
+
         setContent {
             NotificationNoteTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                    data class Note(
-                        val id: Int,
-                        val title: String,
-                        val content: String,
-                        val time: Int,
-                    )
-
                     val notes = remember { mutableStateListOf(
-                        Note(0, "My Title 0", "My content 0", 0),
-                        Note(1, "My Title 1", "My content 1", 1),
+                        Note(10, "My Title 0", "My content 0", 10),
+                        Note(11, "My Title 1", "My content 1", 11),
                     )}
 
+                    LaunchedEffect(1) {
+                        notes.forEach {
+                            postNotificationNote(it)
+                        }
+                    }
+
                     Column {
+                        val extras = intent.extras
+                        val keys = extras?.keySet()
+                        keys?.forEach {
+                            println(it)
+                            println("str: ${extras.getString(it)}")
+                            println("int: ${extras.getInt(it)}")
+                        }
+
+                        println(intent.extras)
                         Greeting(
-                            name = "Android",
+                            name = intent.getIntExtra(EXTRA_NOTIFICATION_ID, -1).toString(),
                             modifier = Modifier.padding(innerPadding)
                         )
 
@@ -79,46 +105,25 @@ class MainActivity : ComponentActivity() {
                             }
                         )
 
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            Button(
-                                onClick = {
-                                    // https://developer.android.com/develop/ui/views/notifications/channels
-
-                                    createNotificationChannel()
-                                }
-                            ) {
-                                Text(stringResource(R.string.create_notification_channel))
-                            }
-                        }
-
                         Button(
                             onClick = {
                                 // https://developer.android.com/develop/ui/views/notifications/build-notification
 
                                 Toast.makeText(this@MainActivity, "Post notification", Toast.LENGTH_SHORT).show()
 
-                                val noteTitle = textFieldStateTitle.text
-                                val noteContent = textFieldStateContent.text
+                                val noteTitle = textFieldStateTitle.text.toString()
+                                val noteContent = textFieldStateContent.text.toString()
 
-                                val builder = NotificationCompat.Builder(this@MainActivity, CHANNEL_ID)
-                                    .setSmallIcon(R.drawable.notification_note_24)
-                                    .setContentTitle(noteTitle)
-                                    .setContentText(noteContent)
-                                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                                val note = Note(
+                                    id = Random.nextInt(),
+                                    title = noteTitle,
+                                    content = noteContent,
+                                    time = 0
+                                )
 
-                                with(NotificationManagerCompat.from(this@MainActivity)) {
-                                    if (ActivityCompat.checkSelfPermission(
-                                            this@MainActivity,
-                                            Manifest.permission.POST_NOTIFICATIONS
-                                        ) != PackageManager.PERMISSION_GRANTED
-                                    ) {
-                                        Toast.makeText(this@MainActivity, "No permission", Toast.LENGTH_SHORT).show()
+                                notes.add(note)
 
-                                        return@with
-                                    }
-                                    // notificationId is a unique int for each notification that you must define.
-                                    notify(0, builder.build())
-                                }
+                                postNotificationNote(note)
                             }
                         ) {
                             Text(stringResource(R.string.send_notification))
@@ -130,7 +135,7 @@ class MainActivity : ComponentActivity() {
                                 key = { it.id }
                             ) { note ->
                                 ListItem(
-                                    leadingContent = {
+                                    overlineContent = {
                                         Text(note.id.toString())
                                     },
                                     headlineContent = {
@@ -148,8 +153,43 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun postNotificationNote(note: Note) {
+        // https://developer.android.com/develop/ui/views/notifications/build-notification#Actions
+        val ACTION_SNOOZE = "snooze"
+
+        val snoozeIntent = Intent(this, MainActivity::class.java).apply {
+            action = ACTION_SNOOZE
+            putExtra(EXTRA_NOTIFICATION_ID, note.id)
+        }
+        val pendingIntent: PendingIntent
+            = PendingIntent.getActivity(this, note.id, snoozeIntent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(this@MainActivity, CHANNEL_ID)
+            .setSmallIcon(R.drawable.notification_note_24)
+            .setContentTitle(note.title)
+            .setContentText(note.content)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .addAction(R.drawable.notification_note_24, "my action ${note.id}", pendingIntent)
+
+        with(NotificationManagerCompat.from(this@MainActivity)) {
+            if (ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(this@MainActivity, "No permission", Toast.LENGTH_SHORT).show()
+
+                return@with
+            }
+            // notificationId is a unique int for each notification that you must define.
+            notify(note.id, builder.build())
+        }
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel() {
+        // https://developer.android.com/develop/ui/views/notifications/channels
+
         Toast.makeText(this@MainActivity, "Create notification channel", Toast.LENGTH_SHORT).show()
         // Create the NotificationChannel.
         val name = getString(R.string.channel_name)
